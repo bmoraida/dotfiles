f053bd9dd7da748d81d144d373c96458e4e21d80
Brenton Moraida <brentonmoraida@protonmail.com>
2019-05-20 13:19:55 -0500

    Added more files and updated existing config

---
+++ .scripts/i3cmds/fuzzy_lock.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Take a screenshot
+scrot /tmp/screen_locked.png
+
+# Pixellate it 10x
+mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i /tmp/screen_locked.png & sleep 5 && xset dpms force off
+
